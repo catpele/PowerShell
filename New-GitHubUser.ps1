14c992b871197c4043a9137d1227bceac7cc710c
Chris Davis <cdavis.home@gmail.com>
2017-12-07 00:14:51 +0000

    Update New-GitHubUser.ps1

--- New-GitHubUser.ps1
+++ New-GitHubUser.ps1
@@ -2,12 +2,12 @@
 # PowerShell GitHub Invites #
 #############################
 
-Write-Output "This script adds a giffgaffer's GitHub account to the giffgaff GitHub organisation and stores their GitHub username in AD within the Info attribute"
+Write-Output "This script invites a GitHub user to an organisation and stores their GitHub username in AD within the Info attribute"
 
 $ADUser = Read-Host "Enter AD Username"
 $GitUser = Read-Host "Enter GitHub Username"
-$GitOrg = "giffgaff"	#Read-Host "Enter GitHub Organisation"
-$GitAccessToken = '?access_token=a223467d9edfe208ddb51ccc33798556d74c6621' #Read-Host "Enter Access Token"
+$GitOrg = Read-Host "Enter GitHub Organisation"
+$GitAccessToken = Read-Host "Enter Access Token"
 $GitApiURL = "https://api.github.com/orgs/$GitOrg/"
 $GitMemberships = "memberships/"
 
